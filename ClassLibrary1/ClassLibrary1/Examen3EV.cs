4d1144eb5f8815f87a98b910acb130507dbea958
hpedreno <pedreno20833@iesmarenostrum.com>
2021-06-08 19:15:11 +0200

    Pregunta 2: A,B,E

--- ClassLibrary1/ClassLibrary1/Examen3EV.cs
+++ ClassLibrary1/ClassLibrary1/Examen3EV.cs
@@ -1,72 +1,165 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace Examen3EV_NS
 {
-    public class estNot  // esta clase nos calcula las estadísticas de un conjunto de notas 
+    /// <summary>
+    /// Esta clase nos calcula las estadísticas de un conjunto de notas 
+    /// Defines the <see cref="EstablecerNota" />.
+    /// </summary>
+    public class EstablecerNota
     {
-        public int sus;  // Suspensos
-        public int apr;  // Aprobados
-        public int not;  // Notables
-        public int sbr;  // Sobresalientes
+        /// <summary>
+        /// Mensaje de error en caso de lista vacía
+        /// </summary>
+        public string errorListaVacia = "Error: La lista no contiene elementos";
 
-        public double media; // Nota media
+        /// <summary>
+        /// Mensaje de error en caso de notas menores a 0 o mayores a 10
+        /// </summary>
+        public string errorNotasIncorrectas = "Error: Las notas deben estar entre 0 y 10, ambos inclusive";
 
-        // Constructor vacío
-        public estNot()
+        /// <summary>
+        /// Representa el contador de suspensos
+        /// </summary>
+        private int suspenso;
+
+        /// <summary>
+        /// Representa el contador de aprobados
+        /// </summary>
+        private int aprobado;
+
+        /// <summary>
+        /// Representa el contador de notables
+        /// </summary>
+        private int notable;
+
+        /// <summary>
+        /// Representa el contador sobresalientes
+        /// </summary>
+        private int sobresaliente;
+
+        /// <summary>
+        /// Representa la nota media;
+        /// </summary>
+        private double notaMedia;
+
+        /// <summary>
+        /// Método Getter
+        /// </summary>
+        /// <returns>Devuelve el contador de suspensos</returns>
+        public int GetSuspenso()
         {
-            sus = apr = not = sbr = 0;  // inicializamos las variables
-            media = 0.0;
+            return suspenso;
+        }
+        /// <summary>
+        /// Método Getter
+        /// </summary>
+        /// <returns>Devuelve el contador de aprobados</returns>
+        public int GetAprobado()
+        {
+            return aprobado;
+        }
+        /// <summary>
+        /// Método Getter
+        /// </summary>
+        /// <returns>Devuelve el contador de notables</returns>
+        public int GetNotable()
+        {
+            return notable;
+        }
+        /// <summary>
+        /// Método Getter
+        /// </summary>
+        /// <returns>Devuelve el contador de sobresalientes</returns>
+        public int GetSobresaliente()
+        {
+            return sobresaliente;
+        }
+        /// <summary>
+        /// Método Getter
+        /// </summary>
+        /// <returns>Devuelve la nota media</returns>
+        public double GetNotaMedia()
+        {
+            return notaMedia;
         }
 
-        // Constructor a partir de un array, calcula las estadísticas al crear el objeto
-        public estNot(List<int> listnot)
+        /// <summary>
+        /// Constructor por defecto
+        /// <para>Se establece una nota media de 0 por defecto</para>
+        /// Initializes a new instance of the <see cref="EstablecerNota"/> class.
+        /// </summary>
+        public EstablecerNota()
         {
-            media = 0.0;
+            this.suspenso = 0;
+            this.aprobado = 0;
+            this.notable = 0;
+            this.sobresaliente = 0;
 
-            for (int i = 0; i < listnot.Count; i++)
-            {
-                if (listnot[i] < 5) sus++;              // Por debajo de 5 suspenso
-                else if (listnot[i] > 5 && listnot[i] < 7) apr++;// Nota para aprobar: 5
-                else if (listnot[i] > 7 && listnot[i] < 9) not++;// Nota para notable: 7
-                else if (listnot[i] > 9) sbr++;         // Nota para sobresaliente: 9
-
-                media = media + listnot[i];
-            }
-
-            media = media / listnot.Count;
+            this.notaMedia = 0.0;
         }
 
+        /// <summary>
+        /// Constructor a partir de un array.
+        /// </summary>
+        /// <remarks>Este constructor llama a su vez a la función "CalcularEstadistica()"</remarks>
+        /// <param name="arrayNotas">The arrayNotas<see cref="List{int}"/>Array de notas</param>
+        /// <return>Devuelve la nota media</return>
+        public EstablecerNota(List<int> arrayNotas)
+        {
+            CalcularEstadistica(arrayNotas);
+        }
 
-        // Para un conjunto de listnot, calculamos las estadísticas
-        // calcula la media y el número de suspensos/aprobados/notables/sobresalientes
-        //
-        // El método devuelve -1 si ha habido algún problema, la media en caso contrario	
-        public double calcEst(List<int> listnot)
-        {                                 
-            media = 0.0;
-
+        /// <summary>
+        /// <para>Mediante el Array de Notas pasado por parámetro calculamos las estadísticas.</para>
+        /// <para>Calcula la notaMedia y el número de suspensos/aprobados/notables/sobresalientes</para>
+        /// </summary>
+        /// <param name="arrayNotas">The arrayNotas<see cref="List{int}"/>Listado de notas</param>
+        /// <exception cref="System.ArgumentOutOfRangeException">La lista no puede estar vacía.</exception>
+        /// <returns>The <see cref="double"/>Devuelve la nota media</returns>
+        public double CalcularEstadistica(List<int> arrayNotas)
+        {
             // TODO: hay que modificar el tratamiento de errores para generar excepciones
             //
-            if (listnot.Count <= 0)  // Si la lista no contiene elementos, devolvemos un error
-                return -1;
-
-            for (int i=0;i<10;i++)
-                if (listnot[i] < 0 || listnot[i] > 10)      // comprobamos que las not están entre 0 y 10 (mínimo y máximo), si no, error
-                return -1;
-
-            for (int i = 0; i < listnot.Count; i++)
+            if (arrayNotas.Count <= 0)  // Si la lista no contiene elementos, devolvemos un error
             {
-                if (listnot[i] < 5) sus++;              // Por debajo de 5 suspenso
-                else if (listnot[i] >= 5 && listnot[i] < 7) apr++;// Nota para aprobar: 5
-                else if (listnot[i] >= 7 && listnot[i] < 9) not++;// Nota para notable: 7
-                else if (listnot[i] > 9) sbr++;         // Nota para sobresaliente: 9
-
-                media = media + listnot[i];
+                throw new ArgumentException(this.errorListaVacia);
             }
 
-            media = media / listnot.Count;
+            for (int i = 0; i < 10; i++)
+            {
+                if (arrayNotas[i] < 0 || arrayNotas[i] > 10)  // comprobamos que las notas están entre 0 y 10 (mínimo y máximo), si no, error
+                {
+                    throw new ArgumentException(this.errorNotasIncorrectas);
+                }
+            }
 
-            return media;
+            for (int i = 0; i < arrayNotas.Count; i++)
+            {
+                if (arrayNotas[i] < 5)
+                {
+                    this.suspenso++;        // Por debajo de 5 suspenso
+                }
+                else if (arrayNotas[i] >= 5 && arrayNotas[i] < 7)
+                {
+                    this.aprobado++;        // Nota para un aprobado: entre 5 y 7
+                }
+                else if (arrayNotas[i] >= 7 && arrayNotas[i] < 9)
+                {
+                    this.notable++;         // Nota para un notable: entre 7 y 9
+                }
+                else if (arrayNotas[i] > 9)
+                {
+                    this.sobresaliente++;   // Nota para sobresaliente: 9 o más
+                }
+
+                this.notaMedia += arrayNotas[i];
+            }
+
+            this.notaMedia /= arrayNotas.Count;
+
+            return this.notaMedia;
         }
     }
 }
